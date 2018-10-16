ab84120e494ededafc13c63a945c5eee1a50f5f2
Christopher Perry <c.perry@icloud.com>
2018-10-16 16:05:56 -0700

    finished

--- src/index.js
+++ src/index.js
@@ -1,1 +1,65 @@
-alert("it's alive");
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const stateInfo = (
+    state1,
+    population1,
+    capital1,
+    state2,
+    population2,
+    capital2,
+    state3,
+    population3,
+    capital3,
+    state4,
+    population4,    
+    capital4
+  ) => {
+    return (
+        <table>
+            <tr>
+                <th>State</th>
+                <th>Population</th>
+                <th>Capital</th>
+            </tr>
+            <tr>
+                <td>{state1}</td>
+                <td>{population1}</td>
+                <td>{capital1}</td>
+            </tr>
+            <tr>
+                <td>{state2}</td>
+                <td>{population2}</td>
+                <td>{capital2}</td>
+            </tr>
+            <tr>
+                <td>{state3}</td>
+                <td>{population3}</td>
+                <td>{capital3}</td>
+            </tr>
+            <tr>
+                <td>{state4}</td>
+                <td>{population4}</td>
+                <td>{capital4}</td>
+            </tr>
+        </table>
+    );
+  };
+  
+  ReactDOM.render(
+    stateInfo(
+      'Idaho',
+      ' ',
+      'Boise',
+      'Tennessee',
+      '6.651 million',
+      'Nashville',
+      'Maine',
+      '1.331 million',
+      'Agusta',
+      'Wisconsin',
+      '5.779 million',
+      'Madison'
+    ),
+    document.getElementById('root')
+  );
